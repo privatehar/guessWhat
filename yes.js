55c0b9a0bee67a5802047eb22c9f3c38ffe2ee38
privatehar <115873494+privatehar@users.noreply.github.com>
2025-02-09 04:57:18 +0530

    Update yes.js

--- yes.js
+++ yes.js
@@ -43,6 +43,8 @@ const febHolidays = [
     "Love You 3000❤️"
   ];
   const ulEl = document.querySelector("ul");
+  const d = new Date();
+  let daynumber = d.getMonth() == 1 ? d.getDate() - 1 : 0;
   let activeIndex = 1;
   const rotate = -360 / febHolidays.length;
   init();
